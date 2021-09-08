435a934ed13578ae9f952b39259f2cfa814c1102
Henrique Sobral <henriquesobral_12@hotmail.com>
2021-09-08 22:45:59 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 // Configurar CORS
 //app.use(express.json())
-app.use(express.static(__dirname + '/'));
+app.use(express.static('client/build'));
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
     res.header('Access-Control-Allow-Origin', '*');
